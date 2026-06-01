1e2eebce9d88bd89e887f32bb2b9ade2e1239057
uglybrecke <206612742+uglybrecke@users.noreply.github.com>
2026-06-01 11:49:46 -0700

    what we worked on in class

--- src/Traversal.java
+++ src/Traversal.java
@@ -1,5 +1,58 @@
 public class Traversal {
   public static void main(String[] args) {
+    TreeNode root = new TreeNode(10);
 
+    root.left = new TreeNode(9);
+    root.left.left = new TreeNode(5);
+    root.left.right = new TreeNode(2);
+
+
+    root.right = new TreeNode(15,
+                            new TreeNode(-3), 
+                            new TreeNode(5, 
+                                        null, 
+                                        new TreeNode(22)
+                                        )
+                            );
+    System.out.println("preorder:");
+    preorder(root);
+    
+    System.out.println("postorder:");
+    postorder(root);
+
+    System.out.println("in order:");
+    inorder(root);
   }
-}
+  /*
+  **************************************************** END OF MAIN ****************************************************
+  */
+
+  //prints all nodes in a preorder way
+  public static void preorder(TreeNode current) {
+    if(current == null) return;
+
+    System.out.println(current.value);
+
+    // explore left (a method that calls itself, recursive)
+    preorder(current.left);
+    // explore right ()
+    preorder(current.right);
+    }
+
+    //if you need information about the smaller nodes first, ie an image in html, can't put a box around it
+    public static void postorder(TreeNode current) {
+      if(current == null) return;
+      postorder(current.left);
+      postorder(current.right);
+      System.out.println(current.value);
+      }
+
+
+    //left -> root -> right
+    public static void inorder(TreeNode current) {
+      if(current == null) return;
+      inorder(current.left);
+      System.out.println(current.value);
+      inorder(current.right);
+    }
+  }
--- src/TreeNode.java
+++ src/TreeNode.java
@@ -1,4 +1,32 @@
 public class TreeNode {
+    public int value;
+    public TreeNode left;
+    public TreeNode right;
 
+    //base constructor
+    public TreeNode(int value, TreeNode left, TreeNode right) {
+        this.value = value;
+        this.left = left;
+        this.right = right;
+    }
+
+    //leaf nodes, null children
+    public TreeNode(int value) {
+        this.value = value;
+    }
+
+    // instead of building a bunch of overloaded methods(same name different types) 
+    // just use:
+    // public myNode = new TreeNode(value, null, TreeNode left)
+
+    // public TreeNode(int value, TreeNode left) {
+    //     this.value = value;
+    //     this.left = left;
+    // }
+
+    // public TreeNode(int value, TreeNode left) {
+    //     this.value = value;
+    //     this.left = left;
+    // }
   
 }
